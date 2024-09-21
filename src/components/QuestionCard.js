7f9ffe002c6dd9e52f0aac218475359133747b50
Samuel Pham <phamquan22042000@gmail.com>
2024-09-21 07:13:40 +0700

    Udacity React - add file

---
+++ src/components/QuestionCard.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+const QuestionCard = () => {
+    return (
+        <div>This is question card component</div>
+    );
+}
+
+export default QuestionCard;
