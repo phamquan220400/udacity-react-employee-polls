b3212578c860f3173f831b858d5938aea738180a
Samuel Pham <phamquan22042000@gmail.com>
2024-10-01 18:32:11 +0700

    Udacity React - update code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import './App.css';
-import {Routes, BrowserRouter as Router, Route} from 'react-router-dom'
+import {Routes, BrowserRouter as Router, Route, Outlet} from 'react-router-dom'
 import Login from "./pages/Login";
 import {Provider} from "react-redux";
 import store from "./store/store";
 import HomePage from "./pages/HomePage";
+import Menu from "./pages/Menu";
+
+function Auth(){
+    return (<Menu><Outlet/></Menu>)
+}
 
 function App() {
     return (
         <Provider store={store}>
+
             <Router>
                 <Routes>
-                    <Route path="/login" element={<Login/>}/>
-                    <Route path="/" element={<HomePage/>}/>
-                    <Route path="/home" element={<HomePage/>}/>
-
+                    <Route element={<Auth/>}>
+                        <Route path="/login" element={<Login/>}/>
+                        <Route path="/" element={<HomePage/>}/>
+                        <Route path="/home" element={<HomePage/>}/>
+                    </Route>
                 </Routes>
             </Router>
         </Provider>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,37 +1,17 @@
 import {useSelector} from "react-redux";
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Tab from '@mui/material/Tab';
-import TabContext from '@mui/lab/TabContext';
-import TabList from '@mui/lab/TabList';
-import TabPanel from '@mui/lab/TabPanel';
-import {useState} from "react";
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import Login from "./Login";
+import LeaderBoard from "./LeaderBoard";
+import NewQuestion from "./NewQuestion";
+
 
 const auth = (state) => state.auth?.user;
 const question = (state) => state.question;
 
 const HomePage = (props) => {
-    const [value, setValue] = useState('1');
-
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
-
     return (
-        <Box sx={{width: '100%', typography: 'body1'}}>
-            <TabContext value={value}>
-                <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
-                    <TabList onChange={handleChange} aria-label="lab API tabs example">
-                        <Tab label="Item One" value="1"/>
-                        <Tab label="Item Two" value="2"/>
-                        <Tab label="Item Three" value="3"/>
-                    </TabList>
-                </Box>
-                <TabPanel value="1">Item One</TabPanel>
-                <TabPanel value="2">Item Two</TabPanel>
-                <TabPanel value="3">Item Three</TabPanel>
-            </TabContext>
-        </Box>
+        <div>This is home page</div>
     );
 }
 
